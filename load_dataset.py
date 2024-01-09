903d2d3c159124e2044e6613293977f978c4aa72
AnHaechan <mike000529@gmail.com>
2024-01-09 08:24:39 +0000

    load dataset with multiple processes; code changed more apt to how download works

--- load_dataset.py
+++ load_dataset.py
@@ -7,7 +7,8 @@ from datasets import load_dataset
 
 # PILE = "EleutherAI/pile"
 PILE = "monology/pile-uncopyrighted"
-DISK_PATH = "/mnt/disks/persist/pile"
+DISK_PATH = "/mnt/disks/persist2/pile"
+NUM_PROC = 32
 
 def print_examples(num_examples=3, split="validation", category="Github"):
     # Stream to iterate over each sample, without downloading the entire dataset
@@ -19,17 +20,19 @@ def print_examples(num_examples=3, split="validation", category="Github"):
     for _ in range(num_examples):
         print(next(iter(pile_github_stream)))
 
-def download(split):
+def download():
     # Load the whole dataset, as filtering before downloading is not allowed
-    pile_split = load_dataset(PILE, streaming=False, split=split, cache_dir=DISK_PATH)
     
+    pile = load_dataset(PILE, streaming=False, cache_dir=DISK_PATH, num_proc=NUM_PROC)
+
     # filter only Github + ArXiv
-    pile_github_arxiv_split = pile_split.filter(lambda sample: sample['meta']["pile_set_name"] in ["Github", "ArXiv"])
-    pile_github_arxiv_split.save_to_disk(f"{DISK_PATH}/pile_gtihub_arxiv_{split}.hf")
+    for split_name, pile_split in pile.items():
+        # e.g. "train", pile_train
+        pile_github_arxiv_split = pile_split.filter(lambda sample: sample['meta']["pile_set_name"] in ["Github", "ArXiv"])
+        pile_github_arxiv_split.save_to_disk(f"{DISK_PATH}/pile_github_arxiv_{split_name}.hf")
 
 if __name__ == "__main__":
-    print_examples()
-    print_examples(2, "train", "ArXiv")
+    # print_examples()
+    # print_examples(2, "train", "ArXiv")
 
-    for split in ["validation", "training", "test"]:
-        download(split)
+    download()
