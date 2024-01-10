3e2512b6cef73274817706d0179a240ea84fe834
AnHaechan <mike000529@gmail.com>
2024-01-10 06:38:48 +0000

    test loading filtered dataset

--- load_dataset.py
+++ load_dataset.py
@@ -3,7 +3,7 @@
 # sudo python3 load_dataset.py
 # ```
 
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 # PILE = "EleutherAI/pile"
 PILE = "monology/pile-uncopyrighted"
@@ -23,16 +23,26 @@ def print_examples(num_examples=3, split="validation", category="Github"):
 def download():
     # Load the whole dataset, as filtering before downloading is not allowed
     
+    print("loading dataset")
     pile = load_dataset(PILE, streaming=False, cache_dir=DISK_PATH, num_proc=NUM_PROC)
 
     # filter only Github + ArXiv
     for split_name, pile_split in pile.items():
         # e.g. "train", pile_train
+        print(f"filtering and saving {split_name}")
         pile_github_arxiv_split = pile_split.filter(lambda sample: sample['meta']["pile_set_name"] in ["Github", "ArXiv"], num_proc=NUM_PROC)
         pile_github_arxiv_split.save_to_disk(f"{DISK_PATH}/pile_github_arxiv_{split_name}.hf", num_proc=NUM_PROC)
+
+def test_load_from_disk():
+    print("loading from disk")
+    pile_filtered_test = load_from_disk(f"{DISK_PATH}/pile_github_arxiv_test.hf")
+    print(pile_filtered_test)
+    print(pile_filtered_test[0])
 
 if __name__ == "__main__":
     # print_examples()
     # print_examples(2, "train", "ArXiv")
 
-    download()
+    # download()
+
+    test_load_from_disk()
